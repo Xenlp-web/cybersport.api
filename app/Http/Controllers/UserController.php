99960ec6227e8c2f567a3ef0083b18de75ba378f
sunset <valentinasavatorova@MacBook-Air-sunset.local>
2020-10-13 10:28:01 +0300

    Сделал отмену записи на турнир

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,4 +58,25 @@ class UserController extends Controller
             return response()->json(['message' => $e->getMessage(), 'status' => 'error'], 400);
         }
     }
+
+    public function cancelTournamentParticipation(Request $request) {
+        $userId = $request->get('user_id');
+        $tournamentId = $request->get('tournament_id');
+        $gameId = $request->get('game_id');
+        try {
+            DB::table('tournaments_and_users')->where('user_id', $userId)->where('tournament_id', $tournamentId)->delete();
+            $tournamentTickets = Tournaments::select('tickets')->where('id', $tournamentId)->where('ended', 0)->firstOrFail();
+            $game = GamesController::getGameById($gameId);
+            $gameSlug = $game->slug;
+            $tournInfoTable = $gameSlug.'_tournaments_info';
+            $players = DB::table($tournInfoTable)->pluck('current_players')->where('tournament_id', $tournamentId);
+            DB::table($tournInfoTable)->where('tournament_id', $tournamentId)->update(['current_players' => $players->current_players - 1]);
+            $user = User::where('id', $userId)->firstOrFail();
+            $user->tickets = $user->tickets + $tournamentTickets;
+            $user->save();
+            return response()->json(['message' => 'Участие в турнире отменено', 'status' => 'success'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'status' => 'error'], 400);
+        }
+    }
 }
